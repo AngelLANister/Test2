1a567fa815d7835391a78d0edfa3ec1787ecf635
AngelLANister <bezrukandrej2@gmail.com>
2026-02-16 21:40:26 +0700

    Ничего не получается

--- 06K_lesson/test_01_form.py
+++ 06K_lesson/test_01_form.py
@@ -1,29 +1,41 @@
-import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+def test_form_submission():
 
-@pytest.fixture
-driver = webdriver.Edge()
-def fill_field(driver, selector, value):
-    driver.find_element(By.CSS_SELECTOR, selector).send_keys(value)
-def test_green_after_submit(driver):
-driver.get('https://bonigarcia.dev/selenium-webdriver-java/data-types.html')
-fill_field(driver, "input[name='first-name']", 'Иван')
-fill_field(driver, "input[name='last-name']", 'Петров')
-fill_field(driver, "input[name='address']", 'Ленина, 55-3')
-fill_field(driver, "input[name='zip-code']", '')
-fill_field(driver, "input[name='city']", 'Москва')
-fill_field(driver, "input[name='country']", 'Россия')
-fill_field(driver, "input[name='e-mail']", 'test@skypro.com')
-fill_field(driver, "input[name='phone']", '+7985899998787')
-fill_field(driver, "input[name='job-position']", 'QA')
-fill_field(driver, "input[name='company']", 'SkyPro')
-driver.find_element(By.CSS_SELECTOR, [type='submit']).click()
-wait = WebDriverWait(driver, 10)
+    driver = webdriver.Edge()
 
-border_color = value_of_css_property("border-color")
-assert border_color == "badbcc"
+    driver.get("https://"
+               "bonigarcia.dev/selenium-webdriver-java/data-types.html")
+    driver.find_element(By.NAME, "first-name").send_keys("Иван")
+    driver.find_element(By.NAME, "last-name").send_keys("Петров")
+    driver.find_element(By.NAME, "address").send_keys("Ленина, 55-3")
+    driver.find_element(By.NAME, "e-mail").send_keys("test@skypro.com")
+    driver.find_element(By.NAME, "phone").send_keys("+7985899998787")
+    driver.find_element(By.NAME, "city").send_keys("Москва")
+    driver.find_element(By.NAME, "country").send_keys("Россия")
+    driver.find_element(By.NAME, "job-position").send_keys("QA")
+    driver.find_element(By.NAME, "company").send_keys("SkyPro")
+    submit_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "[type='submit']"))
+    )
+    submit_button.click()
+
+    zip_code_field = driver.find_element(By.ID, "zip-code")
+    assert ("alert-danger"
+            in zip_code_field.get_attribute("class")), \
+        "Zip code не подсвечен красным"
+
+    green_fields = [
+        "first-name", "last-name", "address", "e-mail", "phone",
+        "city", "country", "job-position", "company"
+    ]
+    for field_id in green_fields:
+        field = driver.find_element(By.ID, field_id)
+        assert "alert-success" in field.get_attribute("class"), \
+            f"Поле {field_id} не подсвечено зеленым"
+
+    driver.quit()
